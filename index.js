12c83167eff8d9327dd48783d21e65aee27061d1
Bartosz <bartosz.gicala@gmail.com>
2024-08-26 16:11:06 +0200

    vercel deploing

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const port = process.env.PORT || 5555;
 let articles = [];
 
-const corsOptions = {
+const corsOption = {
     origin: process.env.PORT,
     optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+}
+app.use(cors(corsOption));
 app.use(express.json());
 
 app.get('/', (req, res) => {
